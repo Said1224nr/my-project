522e1b493bd8a2dbd2a228a78768eccc3482719b
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-04-14 19:18:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let Ism = prompt('Ismingizni kiriting')
+
+let Familiya = prompt('Familiyangizni kiriting')
+
+let Yosh = +prompt('Yoshingizni kiriting')
+
+
+console.log(`Foydalanuvchi ismi:${Ism}\nFoydalanuvchi Familiyasi:${Familiya}\nFoydalanuvchi yoshi:${Yosh}`);
